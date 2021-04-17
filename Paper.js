c783f8e7f0f7c3bd02d32a58d6e0679cae4db887
Ashleywowd <75246779+Ashleywowd@users.noreply.github.com>
2021-04-17 11:34:22 +0300

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,29 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0,
+            density:1.2
+        }
+
+        this.body= Bodies.circle(x,y,r,options)
+        this.r = r
+        World.add(world,this.body)
+        this.image = loadImage("paper.png")
+	}
+    
+    display()
+    {
+        var pos = this.body.position
+        push()
+        translate(pos.x,pos.y)
+        rotate(this.body.angle)
+        imageMode(CENTER)
+        image(this.image,0,0,2*this.r,2*this.r)
+        pop()
+    }
+}
+    
